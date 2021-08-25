d07bc907fcb8afc76f41afdd75974acb8166350e
ggnene <jiin4994@gmail.com>
2021-08-26 01:28:09 +0900

    이미지 url 기능 구현

--- static/js/script.js
+++ static/js/script.js
@@ -1,3 +1,161 @@
+/* 옷 주소 */
+
+// <!--민소매-->
+var A = new Array();
+A[0] = "https://images.unsplash.com/photo-1625127087827-21aec0cf6fc6?ixid=MnwxMjA3fDB8MHxwaG90by1wYWdlfHx8fGVufDB8fHx8&ixlib=rb-1.2.1&auto=format&fit=crop&w=2734&q=80";
+A[1] = "https://images.unsplash.com/photo-1516669305062-08e056ae8098?ixid=MnwxMjA3fDB8MHxwaG90by1wYWdlfHx8fGVufDB8fHx8&ixlib=rb-1.2.1&auto=format&fit=crop&w=2734&q=80";
+A[2] = "https://images.unsplash.com/photo-1566207274740-0f8cf6b7d5a5?ixid=MnwxMjA3fDB8MHxwaG90by1wYWdlfHx8fGVufDB8fHx8&ixlib=rb-1.2.1&auto=format&fit=crop&w=2734&q=80";
+
+<!--가디건-->
+var B = new Array();
+B[0] = "https://images.unsplash.com/photo-1610288311735-39b7facbd095?ixlib=rb-1.2.1&ixid=MnwxMjA3fDB8MHxwaG90by1wYWdlfHx8fGVufDB8fHx8&auto=format&fit=crop&w=2851&q=80";
+B[1] = "https://images.unsplash.com/photo-1623082819157-25d7e8877ffc?ixid=MnwxMjA3fDB8MHxwaG90by1wYWdlfHx8fGVufDB8fHx8&ixlib=rb-1.2.1&auto=format&fit=crop&w=668&q=80";
+B[2] = "https://images.unsplash.com/photo-1616821074314-245674337a0d?ixid=MnwxMjA3fDB8MHxwaG90by1wYWdlfHx8fGVufDB8fHx8&ixlib=rb-1.2.1&auto=format&fit=crop&w=2734&q=80";
+
+<!--긴팔-->
+var C = new Array();
+C[0] = "https://images.unsplash.com/photo-1578587018452-892bacefd3f2?ixlib=rb-1.2.1&ixid=MnwxMjA3fDB8MHxwaG90by1wYWdlfHx8fGVufDB8fHx8&auto=format&fit=crop&w=2734&q=80";
+C[1] = "https://images.unsplash.com/photo-1582551272941-9fc3e8db8bd5?ixid=MnwxMjA3fDB8MHxwaG90by1wYWdlfHx8fGVufDB8fHx8&ixlib=rb-1.2.1&auto=format&fit=crop&w=675&q=80";
+C[2] = "https://images.unsplash.com/photo-1620799140408-edc6dcb6d633?ixid=MnwxMjA3fDB8MHxwaG90by1wYWdlfHx8fGVufDB8fHx8&ixlib=rb-1.2.1&auto=format&fit=crop&w=1266&q=80";
+
+<!--니트-->
+var D = new Array();
+D[0] = "https://images.unsplash.com/photo-1605331362342-38ced45011a5?ixid=MnwxMjA3fDB8MHxwaG90by1wYWdlfHx8fGVufDB8fHx8&ixlib=rb-1.2.1&auto=format&fit=crop&w=800&q=80";
+D[1] = "https://images.unsplash.com/photo-1536992266094-82847e1fd431?ixlib=rb-1.2.1&ixid=MnwxMjA3fDB8MHxwaG90by1wYWdlfHx8fGVufDB8fHx8&auto=format&fit=crop&w=998&q=80";
+D[2] = "https://images.unsplash.com/photo-1485231183945-fffde7cc051e?ixid=MnwxMjA3fDB8MHxwaG90by1wYWdlfHx8fGVufDB8fHx8&ixlib=rb-1.2.1&auto=format&fit=crop&w=668&q=80";
+
+<!--레깅스-->
+var E = new Array();
+E[0] = "https://images.unsplash.com/photo-1596641211273-938aeaf926a9?ixid=MnwxMjA3fDB8MHxwaG90by1wYWdlfHx8fGVufDB8fHx8&ixlib=rb-1.2.1&auto=format&fit=crop&w=668&q=80";
+E[1] = "https://images.unsplash.com/photo-1606902965551-dce093cda6e7?ixid=MnwxMjA3fDB8MHxwaG90by1wYWdlfHx8fGVufDB8fHx8&ixlib=rb-1.2.1&auto=format&fit=crop&w=668&q=80";
+E[2] = "https://images.unsplash.com/photo-1584863495140-a320b13a11a8?ixlib=rb-1.2.1&ixid=MnwxMjA3fDB8MHxwaG90by1wYWdlfHx8fGVufDB8fHx8&auto=format&fit=crop&w=642&q=80";
+
+<!--맨투맨-->
+var F = new Array();
+F[0] = "https://images.unsplash.com/flagged/photo-1575829258379-6f9d5d93eefe?ixlib=rb-1.2.1&ixid=MnwxMjA3fDB8MHxwaG90by1wYWdlfHx8fGVufDB8fHx8&auto=format&fit=crop&w=668&q=80";
+F[1] = "https://images.unsplash.com/photo-1547774475-cc8bce3a768c?ixid=MnwxMjA3fDB8MHxwaG90by1wYWdlfHx8fGVufDB8fHx8&ixlib=rb-1.2.1&auto=format&fit=crop&w=750&q=80";
+F[2] = "https://images.unsplash.com/photo-1582377865071-6e622dc74d8c?ixlib=rb-1.2.1&ixid=MnwxMjA3fDB8MHxwaG90by1wYWdlfHx8fGVufDB8fHx8&auto=format&fit=crop&w=666&q=80";
+
+<!--면바지-->
+var G = new Array();
+G[0] = "https://images.unsplash.com/photo-1616740795271-abd6ce1a5a5a?ixid=MnwxMjA3fDB8MHxwaG90by1wYWdlfHx8fGVufDB8fHx8&ixlib=rb-1.2.1&auto=format&fit=crop&w=716&q=80";
+G[1] = "https://images.unsplash.com/photo-1590159983013-d4ff5fc71c1d?ixid=MnwxMjA3fDB8MHxwaG90by1wYWdlfHx8fGVufDB8fHx8&ixlib=rb-1.2.1&auto=format&fit=crop&w=1000&q=80";
+G[2] = "https://images.unsplash.com/photo-1570653321841-462dd6268561?ixid=MnwxMjA3fDB8MHxwaG90by1wYWdlfHx8fGVufDB8fHx8&ixlib=rb-1.2.1&auto=format&fit=crop&w=938&q=80";
+
+<!--목도리-->
+var H = new Array();
+H[0] = "https://images.unsplash.com/photo-1601498089678-ba42dc32b01b?ixid=MnwxMjA3fDB8MHxwaG90by1wYWdlfHx8fGVufDB8fHx8&ixlib=rb-1.2.1&auto=format&fit=crop&w=668&q=80";
+H[1] = "https://images.unsplash.com/photo-1486132097870-41db1bcd8756?ixid=MnwxMjA3fDB8MHxwaG90by1wYWdlfHx8fGVufDB8fHx8&ixlib=rb-1.2.1&auto=format&fit=crop&w=668&q=80";
+H[2] = "https://images.unsplash.com/photo-1598568290157-bc61638e73f2?ixid=MnwxMjA3fDB8MHxwaG90by1wYWdlfHx8fGVufDB8fHx8&ixlib=rb-1.2.1&auto=format&fit=crop&w=642&q=80";
+
+<!--반바지-->
+var I = new Array();
+I[0] = "https://images.unsplash.com/photo-1473351528942-b35ae8d43e42?ixid=MnwxMjA3fDB8MHxwaG90by1wYWdlfHx8fGVufDB8fHx8&ixlib=rb-1.2.1&auto=format&fit=crop&w=1350&q=80";
+I[1] = "https://images.unsplash.com/photo-1612913334025-bedf136f8715?ixid=MnwxMjA3fDB8MHxwaG90by1wYWdlfHx8fGVufDB8fHx8&ixlib=rb-1.2.1&auto=format&fit=crop&w=666&q=80";
+I[2] = "https://images.unsplash.com/photo-1598522325074-042db73aa4e6?ixid=MnwxMjA3fDB8MHxwaG90by1wYWdlfHx8fGVufDB8fHx8&ixlib=rb-1.2.1&auto=format&fit=crop&w=668&q=80";
+
+<!--반팔-->
+var J = new Array();
+J[0] = "https://images.unsplash.com/photo-1554568218-0f1715e72254?ixlib=rb-1.2.1&ixid=MnwxMjA3fDB8MHxwaG90by1wYWdlfHx8fGVufDB8fHx8&auto=format&fit=crop&w=634&q=80";
+J[1] = "https://images.unsplash.com/photo-1580991584164-a4e12c31521d?ixid=MnwxMjA3fDB8MHxwaG90by1wYWdlfHx8fGVufDB8fHx8&ixlib=rb-1.2.1&auto=format&fit=crop&w=634&q=80";
+J[2] = "https://images.unsplash.com/photo-1503341960582-b45751874cf0?ixid=MnwxMjA3fDB8MHxwaG90by1wYWdlfHx8fGVufDB8fHx8&ixlib=rb-1.2.1&auto=format&fit=crop&w=634&q=80"
+
+// <!--스타킹-->
+var K = new Array();
+K[0] = "https://images.unsplash.com/photo-1606913419164-959745d50efa?ixid=MnwxMjA3fDB8MHxwaG90by1wYWdlfHx8fGVufDB8fHx8&ixlib=rb-1.2.1&auto=format&fit=crop&w=634&q=80";
+K[1] = "https://images.unsplash.com/photo-1555180739-0cb3b1d85138?ixid=MnwxMjA3fDB8MHxwaG90by1wYWdlfHx8fGVufDB8fHx8&ixlib=rb-1.2.1&auto=format&fit=crop&w=634&q=80";
+K[2] = "https://images.unsplash.com/photo-1607081070616-49d27f474a42?ixid=MnwxMjA3fDB8MHxwaG90by1wYWdlfHx8fGVufDB8fHx8&ixlib=rb-1.2.1&auto=format&fit=crop&w=1228&q=80";
+
+<!--야상-->
+var L = new Array();
+L[0] = "https://images.unsplash.com/photo-1510258998236-dd6a60e376a8?ixlib=rb-1.2.1&ixid=MnwxMjA3fDB8MHxwaG90by1wYWdlfHx8fGVufDB8fHx8&auto=format&fit=crop&w=634&q=80";
+L[1] = "https://images.unsplash.com/photo-1592860699405-482d621e2bb1?ixid=MnwxMjA3fDB8MHxwaG90by1wYWdlfHx8fGVufDB8fHx8&ixlib=rb-1.2.1&auto=format&fit=crop&w=634&q=80";
+
+<!--얇은 니트-->
+var M = new Array();
+M[0] = "https://images.unsplash.com/photo-1554355792-be06d064c767?ixid=MnwxMjA3fDB8MHxwaG90by1wYWdlfHx8fGVufDB8fHx8&ixlib=rb-1.2.1&auto=format&fit=crop&w=634&q=80";
+M[1] = "https://images.unsplash.com/photo-1623393807211-3d70dc56395c?ixid=MnwxMjA3fDB8MHxwaG90by1wYWdlfHx8fGVufDB8fHx8&ixlib=rb-1.2.1&auto=format&fit=crop&w=634&q=80";
+M[2] = "https://images.unsplash.com/photo-1554355410-d7f2f6e64e8b?ixid=MnwxMjA3fDB8MHxwaG90by1wYWdlfHx8fGVufDB8fHx8&ixlib=rb-1.2.1&auto=format&fit=crop&w=1350&q=80";
+
+<!--얇은 셔츠-->
+var N = new Array();
+N[0] = "https://images.unsplash.com/photo-1516570693226-2242cec4bfaf?ixid=MnwxMjA3fDB8MHxwaG90by1wYWdlfHx8fGVufDB8fHx8&ixlib=rb-1.2.1&auto=format&fit=crop&w=707&q=80";
+N[1] = "https://images.unsplash.com/photo-1586430921696-c00f6dd64f89?ixid=MnwxMjA3fDB8MHxwaG90by1wYWdlfHx8fGVufDB8fHx8&ixlib=rb-1.2.1&auto=format&fit=crop&w=711&q=80";
+N[2] = "https://images.unsplash.com/photo-1571434305870-0621521af411?ixid=MnwxMjA3fDB8MHxwaG90by1wYWdlfHx8fGVufDB8fHx8&ixlib=rb-1.2.1&auto=format&fit=crop&w=634&q=80";
+
+<!--원피스-->
+var O = new Array();
+O[0] = "https://unsplash.com/photos/68csPWTnafo?utm_source=unsplash&utm_medium=referral&utm_content=creditShareLink";
+O[1] = "https://images.unsplash.com/photo-1571513721963-d855fd8df4c2?ixlib=rb-1.2.1&ixid=MnwxMjA3fDB8MHxwaG90by1wYWdlfHx8fGVufDB8fHx8&auto=format&fit=crop&w=634&q=80";
+O[2] = "https://images.unsplash.com/photo-1575818302670-7e90c19c9082?ixid=MnwxMjA3fDB8MHxwaG90by1wYWdlfHx8fGVufDB8fHx8&ixlib=rb-1.2.1&auto=format&fit=crop&w=634&q=80";
+
+<!--청바지-->
+var P = new Array();
+P[0] = "https://images.unsplash.com/photo-1520517238863-2a437c6b1b08?ixid=MnwxMjA3fDB8MHxwaG90by1wYWdlfHx8fGVufDB8fHx8&ixlib=rb-1.2.1&auto=format&fit=crop&w=1234&q=80";
+P[1] = "https://images.unsplash.com/photo-1616956455145-7c40e34a1c2a?ixlib=rb-1.2.1&ixid=MnwxMjA3fDB8MHxwaG90by1wYWdlfHx8fGVufDB8fHx8&auto=format&fit=crop&w=658&q=80";
+P[2] = "https://images.unsplash.com/photo-1585834142371-f52fa333bf68?ixid=MnwxMjA3fDB8MHxwaG90by1wYWdlfHx8fGVufDB8fHx8&ixlib=rb-1.2.1&auto=format&fit=crop&w=634&q=80";
+
+<!--트렌치코트-->
+var Q = new Array();
+Q[0] = "https://images.unsplash.com/photo-1617639432216-592107059584?ixid=MnwxMjA3fDB8MHxwaG90by1wYWdlfHx8fGVufDB8fHx8&ixlib=rb-1.2.1&auto=format&fit=crop&w=634&q=80";
+Q[1] = "https://images.unsplash.com/photo-1622373349450-1185d8359ea1?ixlib=rb-1.2.1&ixid=MnwxMjA3fDB8MHxwaG90by1wYWdlfHx8fGVufDB8fHx8&auto=format&fit=crop&w=634&q=80";
+Q[2] = "https://images.unsplash.com/photo-1592327877233-90b9bfd92e48?ixlib=rb-1.2.1&ixid=MnwxMjA3fDB8MHxwaG90by1wYWdlfHx8fGVufDB8fHx8&auto=format&fit=crop&w=675&q=80";
+
+<!--패딩-->
+var R = new Array();
+R[0] = "https://images.unsplash.com/photo-1574722772581-f4a4e07e147b?ixlib=rb-1.2.1&ixid=MnwxMjA3fDB8MHxwaG90by1wYWdlfHx8fGVufDB8fHx8&auto=format&fit=crop&w=605&q=80";
+R[1] = "https://images.unsplash.com/photo-1578948856697-db91d246b7b1?ixid=MnwxMjA3fDB8MHxwaG90by1wYWdlfHx8fGVufDB8fHx8&ixlib=rb-1.2.1&auto=format&fit=crop&w=634&q=80";
+R[2] = "https://images.unsplash.com/photo-1568745663067-9248ee880a3b?ixlib=rb-1.2.1&ixid=MnwxMjA3fDB8MHxwaG90by1wYWdlfHx8fGVufDB8fHx8&auto=format&fit=crop&w=675&q=80";
+
+<!--가죽자켓-->
+var S = new Array();
+S[0] = "https://images.unsplash.com/photo-1607892381244-0d9ac5533bc2?ixid=MnwxMjA3fDB8MHxwaG90by1wYWdlfHx8fGVufDB8fHx8&ixlib=rb-1.2.1&auto=format&fit=crop&w=934&q=80";
+S[1] = "https://images.unsplash.com/photo-1520783442300-2f83600761c8?ixid=MnwxMjA3fDB8MHxwaG90by1wYWdlfHx8fGVufDB8fHx8&ixlib=rb-1.2.1&auto=format&fit=crop&w=1350&q=80";
+S[2] = "https://images.unsplash.com/photo-1529882035027-7dcccf0226f4?ixid=MnwxMjA3fDB8MHxwaG90by1wYWdlfHx8fGVufDB8fHx8&ixlib=rb-1.2.1&auto=format&fit=crop&w=939&q=800";
+
+S[3] = "https://images.unsplash.com/photo-1606689288233-96192d451cc3?ixid=MnwxMjA3fDB8MHxzZWFyY2h8MzR8fGphY2tldHxlbnwwfHwwfHw%3D&ixlib=rb-1.2.1&auto=format&fit=crop&w=1100&q=60";
+S[4] = "https://images.unsplash.com/photo-1553527378-5f33c2344855?ixid=MnwxMjA3fDB8MHxzZWFyY2h8NTZ8fGNvYXR8ZW58MHx8MHx8&ixlib=rb-1.2.1&auto=format&fit=crop&w=1100&q=60";
+
+<!--바람막이-->
+var T = new Array();
+T[0] = "https://images.unsplash.com/photo-1604025677169-6b07c4b5d3c3?ixid=MnwxMjA3fDB8MHxwaG90by1wYWdlfHx8fGVufDB8fHx8&ixlib=rb-1.2.1&auto=format&fit=crop&w=800&q=80";
+T[1] = "https://images.unsplash.com/photo-1470611398812-f32f4afdeba0?ixid=MnwxMjA3fDB8MHxwaG90by1wYWdlfHx8fGVufDB8fHx8&ixlib=rb-1.2.1&auto=format&fit=crop&w=1350&q=80";
+T[2] = "https://images.unsplash.com/photo-1598454444341-9b457c01f7ad?ixid=MnwxMjA3fDB8MHxwaG90by1wYWdlfHx8fGVufDB8fHx8&ixlib=rb-1.2.1&auto=format&fit=crop&w=1349&q=80"
+
+// <!--블라우스-->
+var U = new Array();
+U[0] = "https://images.unsplash.com/photo-1558001771-bd9e266b7306?ixid=MnwxMjA3fDB8MHxzZWFyY2h8MTA5fHxibG91c2V8ZW58MHx8MHx8&ixlib=rb-1.2.1&auto=format&fit=crop&w=800&q=60";
+U[1] = "https://images.unsplash.com/photo-1616384037718-103870f436fb?ixid=MnwxMjA3fDB8MHxzZWFyY2h8MTE2fHxibG91c2V8ZW58MHx8MHx8&ixlib=rb-1.2.1&auto=format&fit=crop&w=800&q=60";
+U[2] = "https://images.unsplash.com/photo-1589842526482-2f61badf0827?ixid=MnwxMjA3fDB8MHxzZWFyY2h8MTUxfHxibG91c2V8ZW58MHx8MHx8&ixlib=rb-1.2.1&auto=format&fit=crop&w=700&q=60";
+U[3] = "https://images.unsplash.com/photo-1595828204607-0d09afb58a71?ixid=MnwxMjA3fDB8MHxzZWFyY2h8Mnx8YmxvdXNlfGVufDB8fDB8fA%3D%3D&ixlib=rb-1.2.1&auto=format&fit=crop&w=800&q=60";
+
+<!--스키니-->
+var V = new Array();
+V[0] = "https://images.unsplash.com/photo-1523419951196-9c85a25cd485?ixid=MnwxMjA3fDB8MHxzZWFyY2h8MTE0fHxza2lubnklMjBqZWFuc3xlbnwwfHwwfHw%3D&ixlib=rb-1.2.1&auto=format&fit=crop&w=1100&q=60";
+V[1] = "https://images.unsplash.com/photo-1607039233937-86eb0bef557f?ixid=MnwxMjA3fDB8MHxzZWFyY2h8MTR8fHNraW5ueSUyMGplYW5zfGVufDB8fDB8fA%3D%3D&ixlib=rb-1.2.1&auto=format&fit=crop&w=1100&q=60";
+V[2] = "https://images.unsplash.com/photo-1533825828907-94649041aa67?ixid=MnwxMjA3fDB8MHxzZWFyY2h8OHx8c2tpbm55JTIwamVhbnN8ZW58MHx8MHx8&ixlib=rb-1.2.1&auto=format&fit=crop&w=1100&q=60";
+
+<!--슬렉스-->
+var W = new Array();
+W[0] = "https://images.unsplash.com/photo-1613590171270-cb70d833aa02?ixid=MnwxMjA3fDB8MHxzZWFyY2h8OTR8fHRyb3VzZXJzfGVufDB8fDB8fA%3D%3D&ixlib=rb-1.2.1&auto=format&fit=crop&w=1100&q=60";
+W[1] = "https://images.unsplash.com/photo-1542595734-49653d33e0eb?ixid=MnwxMjA3fDB8MHxzZWFyY2h8MTE3fHx0cm91c2Vyc3xlbnwwfHwwfHw%3D&ixlib=rb-1.2.1&auto=format&fit=crop&w=1100&q=60";
+W[2] = "https://images.unsplash.com/photo-1604136172384-b2e9c43271ec?ixid=MnwxMjA3fDB8MHxzZWFyY2h8NDZ8fHRyb3VzZXJzfGVufDB8fDB8fA%3D%3D&ixlib=rb-1.2.1&auto=format&fit=crop&w=1100&q=60";
+
+<!--코트-->
+var X = new Array();
+X[0] = "https://images.unsplash.com/photo-1614242144275-93dc5724fb07?ixid=MnwxMjA3fDB8MHxzZWFyY2h8MTl8fGNvYXR8ZW58MHx8MHx8&ixlib=rb-1.2.1&auto=format&fit=crop&w=1100&q=60";
+X[1] = "https://images.unsplash.com/photo-1612462766564-895ea3388d2b?ixid=MnwxMjA3fDB8MHxzZWFyY2h8NHx8Y29hdHxlbnwwfHwwfHw%3D&ixlib=rb-1.2.1&auto=format&fit=crop&w=1100&q=60";
+X[2] = "https://images.unsplash.com/photo-1605895640528-66bc5374504e?ixid=MnwxMjA3fDB8MHxzZWFyY2h8MzJ8fGNvYXR8ZW58MHx8MHx8&ixlib=rb-1.2.1&auto=format&fit=crop&w=1100&q=60";
+X[3] = "https://images.unsplash.com/photo-1578102718171-ec1f91680562?ixid=MnwxMjA3fDB8MHxzZWFyY2h8MzB8fGNvYXR8ZW58MHx8MHx8&ixlib=rb-1.2.1&auto=format&fit=crop&w=1100&q=60";
+
+<!--후드-->
+var Y = new Array();
+Y[0] = "https://images.unsplash.com/photo-1575228245458-b25e14dd04a8?ixid=MnwxMjA3fDB8MHxzZWFyY2h8NjJ8fGhvb2R8ZW58MHx8MHx8&ixlib=rb-1.2.1&auto=format&fit=crop&w=1100&q=60";
+Y[1] = "https://images.unsplash.com/photo-1610271340738-726e199f0258?ixid=MnwxMjA3fDB8MHxzZWFyY2h8MTAxfHxob29kfGVufDB8fDB8fA%3D%3D&ixlib=rb-1.2.1&auto=format&fit=crop&w=1100&q=600";
+Y[2] = "https://images.unsplash.com/photo-1578172745579-92c2de4f383a?ixid=MnwxMjA3fDB8MHxzZWFyY2h8ODF8fGhvb2R8ZW58MHx8MHx8&ixlib=rb-1.2.1&auto=format&fit=crop&w=1100&q=60";
+
+
+
 /* OpenWeather API 요청 코드 */
         $.ajax({
             type: "GET",
@@ -86,6 +244,73 @@
                     weatherbg(wt_icon)
 
                 }
+                        /* 썸네일 코드 */
+
+                function random(pram, nb) {
+                    let imgNum = Math.round(Math.random() * pram.length);
+                    let objImg = document.getElementById("introImg"+nb);
+                        objImg.src = pram[imgNum];
+                }
+
+                if (temp <5) {
+                    let tbd1 = R;
+                    let tbd2 = X;
+                    let tbd3 = H;
+                    random(tbd1, 1);
+                    random(tbd2, 2);
+                    random(tbd3, 3);
+                } else if (5 <=temp & temp <9) {
+                    let tbd1 = S;
+                    let tbd2 = D;
+                    let tbd3 = E;
+                    random(tbd1, 1);
+                    random(tbd2, 2);
+                    random(tbd3, 3);
+                } else if (9 <=temp & temp <12) {
+                    let tbd1 = D.concat(L);
+                    let tbd2 = Q;
+                    let tbd3 = P.concat(K);
+                    random(tbd1, 1);
+                    random(tbd2, 2);
+                    random(tbd3, 3);
+                } else if (12 <=temp & temp <17) {
+                    let tbd1 = B.concat(T.concat((L)));
+                    let tbd2 = G.concat(P);
+                    let tbd3 = K;
+                    random(tbd1, 1);
+                    random(tbd2, 2);
+                    random(tbd3, 3);
+                } else if (17 <=temp & temp <20) {
+                    let tbd1 = M.concat(F);
+                    let tbd2 = O.concat((B.concat(Y)));
+                    let tbd3 = W.concat(G.concat(P));
+                    random(tbd1, 1);
+                    random(tbd2, 2);
+                    random(tbd3, 3);
+                } else if (20 <=temp & temp <23) {
+                    let tbd1 = C.concat(U);
+                    let tbd2 = W.concat(Y.concat(B));
+                    let tbd3 = V.concat(P.concat(G));
+                    random(tbd1, 1);
+                    random(tbd2, 2);
+                    random(tbd3, 3);
+                    console.log(tbd1)
+                } else if (23 <=temp & temp <28) {
+                    let tbd1 = J.concat(N.concat(G.concat(I)));
+                    let tbd2 = G;
+                    let tbd3 = I;
+                    random(tbd1, 1);
+                    random(tbd2, 2);
+                    random(tbd3, 3);
+                } else /* over 28 degree*/ {
+                    let tbd1 = A.concat(J);
+                    let tbd2 = I;
+                    let tbd3 = O;
+                    random(tbd1, 1);
+                    random(tbd2, 2);
+                    random(tbd3, 3);
+                }
+
             }
         })
 /*자외선 요청 코드*/
@@ -102,30 +327,3 @@
             }
 
         })
-
-
-/* 썸네일 코드 */
-
-                let A1 = U.concat(W)
-                let A2 = V.concat(U)
-                let A3 = V.concat(U, W);
-
-
-                function showImage1() {
-                    let imgNum = Math.round(Math.random() * A1.length);
-                    let objImg = document.getElementById("introImg1");
-                    objImg.src = A1[imgNum];
-                }
-
-                function showImage2() {
-                    let imgNum = Math.round(Math.random() * A2.length);
-                    let objImg = document.getElementById("introImg2");
-                    objImg.src = A2[imgNum];
-                }
-
-                function showImage3() {
-                    let imgNum = Math.round(Math.random() * A3.length);
-                    let objImg = document.getElementById("introImg3");
-                    objImg.src = A3[imgNum];
-                }
-
